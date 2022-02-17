60938273198dc3721a623ae9e5633de09a17a9fb
WannesVermeire <wannes.vermeiren@student.kuleuven.be>
2022-02-17 21:26:42 +0100

    aanmaak project

---
+++ src/test/java/com/example/BachelorProefBackend/BachelorProefBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BachelorProefBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BachelorProefBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
