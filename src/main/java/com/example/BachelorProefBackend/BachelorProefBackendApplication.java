4f3d563549851b671b6763a4655fa202cf36e0c8
WoutDeleu <80529253+WoutDeleu@users.noreply.github.com>
2022-02-22 15:42:13 +0100

    Server runnen (comment in dependency) + UserFiles

--- src/main/java/com/example/BachelorProefBackend/BachelorProefBackendApplication.java
+++ src/main/java/com/example/BachelorProefBackend/BachelorProefBackendApplication.java
@@ -2,8 +2,10 @@ package com.example.BachelorProefBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BachelorProefBackendApplication {
 
 	public static void main(String[] args) {
