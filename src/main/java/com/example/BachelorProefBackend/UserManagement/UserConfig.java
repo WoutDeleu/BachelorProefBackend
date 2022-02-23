1af4dbf507eea136a30c26498c1e4e23e0266cd0
WoutDeleu <80529253+WoutDeleu@users.noreply.github.com>
2022-02-23 17:41:24 +0100

    Config

--- src/main/java/com/example/BachelorProefBackend/UserManagement/UserConfig.java
+++ src/main/java/com/example/BachelorProefBackend/UserManagement/UserConfig.java
@@ -1,5 +1,6 @@
 package com.example.BachelorProefBackend.UserManagement;
 
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +15,14 @@ public class UserConfig {
     CommandLineRunner commandLineRunner(UserRepository users) {
         return args -> {
             User_entity student1 = new User_entity("Wannes", "Vermeiren", "wannes.vermeiren@hotmail.eu", "0453699356", true, false, false, false);
-            users.saveAll(List.of(student1));
+            User_entity student2 = new User_entity("Toon", "Eeraerts", "toon.eeraerts@hotmail.eu", "0453699356", true, false, false, false);
+            User_entity administrator = new User_entity("Pol", "Thys", "Pol.thys@hotmail.eu", "0453699356", false, true, false, false);
+            User_entity promotor = new User_entity("Alain", "Vandamme", "alain.vandamme@vandammeplastic.eu", "0453699356", false, false, true, false);
+            User_entity coordinator = new User_entity("Michel", "Drets", "michel.drets@vandammeplastic.eu", "0453699356", false, false, false, true);
+
+            User_entity alphamail = new User_entity("Tony", "Wauters", "tony.wauters@kuleuven.be", "0596345698", true, true, true,true);
+
+            users.saveAll(List.of(student1,student2,administrator,promotor,coordinator, alphamail));
         };
     }
 }
