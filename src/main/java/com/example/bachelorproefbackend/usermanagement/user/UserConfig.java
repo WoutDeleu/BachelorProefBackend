13e6e264c063151d1d883ba17c4d15b8b87693e8
WannesVermeire <wannes.vermeiren@student.kuleuven.be>
2022-05-06 20:49:58 +0200

    get subjects doesn't work

--- src/main/java/com/example/bachelorproefbackend/usermanagement/user/UserConfig.java
+++ src/main/java/com/example/bachelorproefbackend/usermanagement/user/UserConfig.java
@@ -44,7 +44,6 @@ public class UserConfig {
             tagService.addNewTag(tag1);
             tagService.addNewTag(tag2);
 
-
             Subject subject1 = new Subject("Vliegtuigen", "Hoe blijven ze in de lucht?", 1);
             Subject subject2 = new Subject("Software project failure", "Hoe zorgen we dat dit project in goede banen loopt?", 1);
             Subject subject3 = new Subject("Nalu studie", "Hoeveel nalu's is te veel voor Toon", 2);
