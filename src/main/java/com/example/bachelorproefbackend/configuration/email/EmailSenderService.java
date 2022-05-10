eb72ff36e61c6198b951ad2b02180b98b9150be6
WannesVermeire <wannes.vermeiren@student.kuleuven.be>
2022-05-10 17:07:33 +0200

    mailing

--- src/main/java/com/example/bachelorproefbackend/configuration/email/EmailSenderService.java
+++ src/main/java/com/example/bachelorproefbackend/configuration/email/EmailSenderService.java
@@ -14,36 +14,36 @@ import java.util.Properties;
 @Slf4j
 @Service
 public class EmailSenderService {
-
-
-    private JavaMailSender mailSender;
-
-//    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-
-        mailSender.setUsername("my.gmail@gmail.com");
-        mailSender.setPassword("password");
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-
-        return mailSender;
-    }
+//
+//
+//    private JavaMailSender mailSender;
+//
+////    @Bean
+//    public JavaMailSender getJavaMailSender() {
+//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//        mailSender.setHost("smtp.gmail.com");
+//        mailSender.setPort(587);
+//
+//        mailSender.setUsername("my.gmail@gmail.com");
+//        mailSender.setPassword("password");
+//
+//        Properties props = mailSender.getJavaMailProperties();
+//        props.put("mail.transport.protocol", "smtp");
+//        props.put("mail.smtp.auth", "true");
+//        props.put("mail.smtp.starttls.enable", "true");
+//        props.put("mail.debug", "true");
+//
+//        return mailSender;
+//    }
 
     public void sendEmail(String to, String subject, String body) {
-        mailSender = getJavaMailSender();
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
-
-        mailSender.send(message);
-        log.info("Mail send successfully...");
+//        mailSender = getJavaMailSender();
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setTo(to);
+//        message.setSubject(subject);
+//        message.setText(body);
+//
+//        mailSender.send(message);
+//        log.info("Mail send successfully...");
     }
 }
